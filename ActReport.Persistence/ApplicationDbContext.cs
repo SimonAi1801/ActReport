8feff5d5fea65eb947a406105fad2045812d423a
MIGU <m.gutenbrunner@itag.at>
2020-10-15 17:16:27 +0200

    Add project files.

---
+++ ActReport.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using ActReport.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Diagnostics;
+
+namespace ActReport.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Core.Entities.Activity> Activities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            var configuration = builder.Build();
+            Debug.Write(configuration.ToString());
+            //string connectionString = configuration["ConnectionStrings:DefaultConnection"];
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ActReportDb;Integrated Security=True;";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
