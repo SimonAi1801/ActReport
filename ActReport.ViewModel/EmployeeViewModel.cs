709016582109b0f1962c4de9a84aab9882dd5fa4
MIGU <m.gutenbrunner@itag.at>
2020-10-16 14:48:58 +0200

    implements Tutorial

---
+++ ActReport.ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,101 @@
+﻿using ActReport.Core.Contracts;
+using ActReport.Core.Entities;
+using ActReport.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace ActReport.ViewModel
+{
+    public class EmployeeViewModel : BaseViewModel
+    {
+        private string _firstName;
+        private string _lastName;
+        private Employee _selectedEmployee;
+        private ObservableCollection<Employee> _employees;
+        private ICommand _cmdSaveChanges;
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged(nameof(FirstName));
+            }
+        }
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged(nameof(LastName));
+            }
+        }
+        public Employee SelectedEmployee
+        {
+            get => _selectedEmployee;
+            set
+            {
+                _selectedEmployee = value;
+                FirstName = _selectedEmployee?.FirstName;
+                LastName = _selectedEmployee?.LastName;
+                OnPropertyChanged(nameof(SelectedEmployee));
+            }
+        }
+        public ObservableCollection<Employee> Employees
+        {
+            get => _employees;
+            set
+            {
+                _employees = value;
+                OnPropertyChanged(nameof(Employee));
+            }
+        }
+        public ICommand CmdSaveChanges
+        {
+            get
+            {
+                if(_cmdSaveChanges == null)
+                {
+                    _cmdSaveChanges = new RelayCommand(
+                        execute: _ =>
+                        {
+                            using IUnitOfWork uow = new UnitOfWork();
+                            _selectedEmployee.FirstName = _firstName;
+                            _selectedEmployee.LastName = _lastName;
+                            uow.EmployeeRepository.Update(_selectedEmployee);
+                            uow.Save();
+
+                            LoadEmployees();
+                        },
+                        canExecute: _ => _selectedEmployee != null);
+                }
+
+                return _cmdSaveChanges;
+            }
+
+            set { _cmdSaveChanges = value; }
+        }
+
+        public EmployeeViewModel()
+        {
+            LoadEmployees();
+        }
+        private void LoadEmployees()
+        {
+            using(UnitOfWork uow = new UnitOfWork())
+            {
+                var employees = uow.EmployeeRepository
+                    .Get(orderBy: coll => coll.OrderBy(emp => emp.LastName))
+                    .ToList();
+
+                Employees = new ObservableCollection<Employee>(employees);
+            }
+        }
+    }
+}
